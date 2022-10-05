2f57712ed5f5bfc734393cbcb996aebc0d111ba7
Anton <69902586+ant0n7@users.noreply.github.com>
2022-10-05 20:13:04 +0200

    Add first pages, styling, bootstrap, navbar.
    Added
    - a home (index.vue) and an about (about.vue) page
    - a navbar
    - scss files for all pages
    - bootstrap

--- client-frontend/nuxt.config.ts
+++ client-frontend/nuxt.config.ts
@@ -3,5 +3,10 @@ export default defineNuxtConfig({
   css: [
     "~/assets/scss/futura.css",
     "~/assets/scss/main.scss", // maybe replace ~ with @
+    "~/assets/scss/about.scss",
+    "~/assets/scss/navbar.scss",
+    "~/assets/scss/footer.scss",
+    "~/assets/scss/home.scss",
+    "~/assets/scss/gallery.scss",
   ],
 });
--- client-frontend/pages/index.vue
+++ client-frontend/pages/index.vue
@@ -1,5 +1,29 @@
 <template>
   <div>
-    <h1>Hello World</h1>
+    <main>
+      <div class="container-fluid" id="lettering-container">
+        <Lettering />
+      </div>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-3"></div>
+          <div class="col-md-6 col-12">
+            <p id="welcome-text">
+              Wir helfen Anwaltskanzleien dabei, ihre Mandantengewinnung durch
+              eine moderne Webseite zu verbessern. Mit unseren durchdachten
+              Techniken verhindern wir, dass potenzielle Mandanten und
+              Mitarbeiter zum Konkurrenten abspringen und hilft beim
+              Vertrauensaufbau von Interessenten.
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
+
+<style scoped>
+#welcome-text {
+  text-align: center;
+}
+</style>
