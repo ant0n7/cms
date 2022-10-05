ffdf42c207389106011e99e990dab44298b4f11f
Anton <69902586+ant0n7@users.noreply.github.com>
2022-10-05 18:26:50 +0200

    Add Futura font.
    Added the css-file for the futura typekit font.

--- client-frontend/nuxt.config.ts
+++ client-frontend/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: [
+    "~/assets/scss/futura.css",
     "~/assets/scss/main.scss", // maybe replace ~ with @
   ],
 });
