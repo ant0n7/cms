c65c8e6f798643cc4eb6f001763df8106412589c
Anton <69902586+ant0n7@users.noreply.github.com>
2022-10-31 22:51:49 +0100

    Implement iframe.
    The iframe displays the customers live website.

--- cms-frontend/pages/index.vue
+++ cms-frontend/pages/index.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
-    <h1>Index</h1>
-    <button @click="logUser">Log user</button>
+    <h1>Welcome</h1>
+
+    <div class="card w-100" style="height: 70vh">
+      <div class="card-body">
+        <h5 class="card-title">Your Website</h5>
+        <iframe
+          src="http://localhost:3000/"
+          class="w-100"
+          frameborder="0"
+        ></iframe>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,3 +28,17 @@ export default Vue.extend({
   },
 })
 </script>
+
+<style lang="scss" scoped>
+iframe {
+  height: 90%;
+  border-radius: 5px;
+
+  body::-webkit-scrollbar {
+    display: none;
+  }
+
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+}
+</style>
