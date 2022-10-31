7d7ebe246cbd4724b688644ca8cd76624d1c11fc
Anton <69902586+ant0n7@users.noreply.github.com>
2022-10-31 23:14:11 +0100

    Changed data access rights.
    Allow everyone to access /api/data via GET-requests.

--- backend/src/main/java/com/example/demo/domain/security/SecurityConfig.java
+++ backend/src/main/java/com/example/demo/domain/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.example.demo.domain.appuser.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -51,6 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.authorizeRequests()
                 .antMatchers("/api/users/login").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/data/**").permitAll()
                 .anyRequest().authenticated();
 
         http.exceptionHandling()
