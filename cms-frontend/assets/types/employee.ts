2d3d785671064260939675f96041455edec3cfb3
Anton <69902586+ant0n7@users.noreply.github.com>
2022-11-01 11:29:53 +0100

    Optimize employee fetch.
    Now showing a list of employees in the frontend.

---
+++ cms-frontend/assets/types/employee.ts
@@ -0,0 +1,10 @@
+// TODO: Extend the Employee type to include a all fields from the backend
+
+type Employee = {
+  firstname: string
+  lastname: string
+  email: string
+  jobTitle: string
+}
+
+export default Employee
