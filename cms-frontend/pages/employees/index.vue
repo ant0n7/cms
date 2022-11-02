1c6840a393aaea51f2b87e5b1187c8d4f2803963
Anton <69902586+ant0n7@users.noreply.github.com>
2022-11-02 18:57:45 +0100

    Create Card component.
    Card component has default bootstrap styling to keep it simple.

--- cms-frontend/pages/employees/index.vue
+++ cms-frontend/pages/employees/index.vue
@@ -3,60 +3,90 @@
     <h1>Employees</h1>
     <p>This section is for display purposes only.</p>
 
-    <div v-if="employees.length > 0" class="row">
-      <div
-        v-for="employee in employees"
-        :key="employee"
-        class="col-md-4 col-12"
-      >
-        <div class="card w-100">
-          <div class="card-body">
-            <h5 class="card-title">
-              {{ employee.firstname }} {{ employee.lastname }}
-            </h5>
-            <!-- Card image -->
-            <img
-              :id="employee.id + '_image'"
-              class="card-img-top"
-              src=""
-              alt="Card image cap"
-            />
-            <p class="card-text">{{ employee.jobTitle }}</p>
+    <client-only>
+      <div v-if="employees.length > 0" class="row">
+        <div
+          v-for="employee in employees"
+          :key="employee.id"
+          class="col-md-4 col-12"
+        >
+          <BasicCard
+            class="mb-4"
+            :title="`${employee.firstname} ${employee.lastname}`"
+            :subtitle="employee.jobTitle"
+            :img-src="
+              'data:image/jpg;base64,' +
+              getEmployeeImage(employee).then((res) => res.toString('base64'))
+            "
+          >
             <p class="card-text">{{ employee.email }}</p>
-          </div>
+          </BasicCard>
         </div>
       </div>
-    </div>
-    <div v-else-if="employees.length == 0">
-      <div class="alert alert-warning" role="alert">No employees found.</div>
-    </div>
-    <div v-else>
-      <div class="alert alert-error" role="alert">Couldn't load employees.</div>
-    </div>
+      <div v-else-if="employees.length == 0">
+        <div class="alert alert-warning" role="alert">No employees found.</div>
+      </div>
+      <div v-else>
+        <div class="alert alert-error" role="alert">
+          Couldn't load employees.
+        </div>
+      </div>
+    </client-only>
   </div>
 </template>
 
 <script lang="ts">
+import { Buffer } from 'buffer'
+import { response } from 'express'
 import Vue from 'vue'
-// import Employee from '~/assets/types/employee'
+// import Employee from assets/types
+// import Employee from '~/types'
+
+class Employee {
+  id?: string
+  firstname?: string
+  lastname?: string
+  email?: string
+  jobTitle?: string
+}
 
 export default Vue.extend({
   // name: 'EmployeesPage',
   data: () => ({
-    employees: [],
+    employees: new Array<Employee>(),
   }),
   async fetch() {
     this.employees = await this.$axios.$get('/data/employees/')
-    this.employees.forEach(async (employee) => {
-      await this.$axios
-        .$get(`/data/employees/${employee.id}/image`)
-        .then((response) => {
-          const imageEncoded = btoa(response)
-          ;(<HTMLImageElement>(
-            document!.getElementById(employee.id + '_image')
-          ))!.src = `data:image/jpg;base64,${imageEncoded}`
-        })
-    })
+    // this.employees.forEach(async (employee: Employee) => {
+    //   await this.$axios
+    //     .$get(`/data/employees/${employee.id}/image`)
+    //     .then((response) => {
+    //       const imageEncoded = Buffer.from(response, 'base64')
+    //       ;(<HTMLImageElement>(
+    //         document!.getElementById(employee.id + '_image')
+    //       ))!.src = `data:image/jpg;base64,${imageEncoded}`
+    //     })
+    // })
+  },
+  methods: {
+    async getEmployeeImage(employee: Employee): Promise<Buffer> {
+      // await this.$axios
+      //   .$get(`/data/employees/${employee.id}/image`)
+      //   .then((response: string) => {
+      //     const imageEncoded = Buffer.from(response, 'base64')
+      //     ;(<HTMLImageElement>(
+      //       document!.getElementById(employee.id + '_image')
+      //     ))!.src = `data:image/jpg;base64,${imageEncoded}`
+      //   })
+      const response = await this.$axios.$get(
+        `/data/employees/${employee.id}/image`
+      )
+
+      return Buffer.from(response)
+    },
+    base64Encode(s: string) {
+      return Buffer.from(s).toString('base64')
+    },
   },
 })
 </script>
