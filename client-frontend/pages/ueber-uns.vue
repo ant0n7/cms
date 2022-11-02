769230f738b43f8914a37d8b4bb415f3b957f9c8
Anton <69902586+ant0n7@users.noreply.github.com>
2022-11-02 22:58:53 +0100

    Amend.

--- client-frontend/pages/ueber-uns.vue
+++ client-frontend/pages/ueber-uns.vue
@@ -1,3 +1,7 @@
+<script setup>
+const { data: employees } = await useFetch("/data/employees");
+</script>
+
 <template>
   <div>
     <PageTitle>&Uuml;ber uns</PageTitle>
@@ -18,7 +22,35 @@
           <div class="col-md-3"></div>
         </div>
         <div class="team">
-          <div class="row team-title-row">
+          <div v-for="employee in employees" :key="employee.id">
+            <div class="row team-title-row">
+              <div class="col-md-3"></div>
+              <div class="col-md-6 col-12">
+                <h2 class="team-title">
+                  {{ employee.firstname }} {{ employee.lastname }}
+                </h2>
+              </div>
+              <div class="col-md-3"></div>
+            </div>
+            <div class="row team-desc-row">
+              <div class="col-md-3"></div>
+              <div class="col-md-2 col-12">
+                <p class="team-job" style="opacity: 50%">
+                  {{ employee.jobTitle }}
+                </p>
+              </div>
+              <div class="col-md-4 col-12">
+                <p class="team-desc">
+                  {{ employee.description }}
+                  <br />
+                  <br />
+                  <a :href="'mailto:' + employee.email">{{ employee.email }}</a>
+                </p>
+              </div>
+              <div class="col-md-3"></div>
+            </div>
+          </div>
+          <!-- <div class="row team-title-row">
             <div class="col-md-3"></div>
             <div class="col-md-6 col-12">
               <h2 class="team-title">Anton Detken</h2>
@@ -28,10 +60,7 @@
           <div class="row team-desc-row">
             <div class="col-md-3"></div>
             <div class="col-md-2 col-12">
-              <img
-                class="team-img"
-                src="/team/detken_anton_portrait.jpg"
-              />
+              <img class="team-img" src="/team/detken_anton_portrait.jpg" />
               <p class="team-job" style="opacity: 50%">
                 Gesch&auml;ftsf&uuml;hrer
               </p>
@@ -51,7 +80,7 @@
               </p>
             </div>
             <div class="col-md-3"></div>
-          </div>
+          </div> -->
         </div>
       </div>
     </main>
@@ -61,21 +90,27 @@
 <script>
 export default {
   head: {
-    title: 'Über uns - Detken Design',
+    title: "Über uns - Detken Design",
     meta: [
       {
-        hid: 'description',
-        name: 'description',
+        hid: "description",
+        name: "description",
         content:
-          'Wir planen und realisieren Webdesign für Anwaltskanzleien. Dabei setzen wir durchdachte Techniken ein, die potenzielle Kunden und Mitarbeiter überzeugen. Dadurch verhindern wir unter anderem, dass Ihre Website-Besucher zu Ihren Konkurrenten abspringen.',
+          "Wir planen und realisieren Webdesign für Anwaltskanzleien. Dabei setzen wir durchdachte Techniken ein, die potenzielle Kunden und Mitarbeiter überzeugen. Dadurch verhindern wir unter anderem, dass Ihre Website-Besucher zu Ihren Konkurrenten abspringen.",
       },
       {
-        hid: 'og:description',
-        name: 'og:description',
+        hid: "og:description",
+        name: "og:description",
         content:
-          'Wir planen und realisieren Webdesign für Anwaltskanzleien. Dabei setzen wir durchdachte Techniken ein, die potenzielle Kunden und Mitarbeiter überzeugen. Dadurch verhindern wir unter anderem, dass Ihre Website-Besucher zu Ihren Konkurrenten abspringen.',
+          "Wir planen und realisieren Webdesign für Anwaltskanzleien. Dabei setzen wir durchdachte Techniken ein, die potenzielle Kunden und Mitarbeiter überzeugen. Dadurch verhindern wir unter anderem, dass Ihre Website-Besucher zu Ihren Konkurrenten abspringen.",
       },
     ],
   },
-}
+  data: () => ({
+    // employees: new Array<Employee>(),
+  }),
+  // async fetch() {
+  //   this.employees = await this.$axios.$get("/data/employees/");
+  // },
+};
 </script>
